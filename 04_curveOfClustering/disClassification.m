38fb6b14818cc2f4db2916fa04177e2276457d14
Xinyu-Liu-Kingsley <eurekaxyliu@163.com>
2020-11-19 23:57:13 +0800

    update cluster and plot pic

--- 04_curveOfClustering/disClassification.m
+++ 04_curveOfClustering/disClassification.m
@@ -1,12 +1,12 @@
 close all
 clear
 clc
-
+tic
 dbstop if error
 % load Distance.mat
 load('distance_2.mat');
 
-MAX_DISTANCE = 1;
+MAX_DISTANCE = 5;
 % init
 Num = length(distance);
 Dist = zeros(Num,Num);
@@ -23,11 +23,11 @@ end
 for i=1:Num
     Dist(i,:) = distance{i}; 
 end
-
+Dist = Dist + Dist';
 % Sort the distance array
 distNum = size(Dist,1);
 for i = 1 : distNum-1
-    [min_dist, min_dist_idx] =  findSecondMin(Dist(i,:),i);
+    [min_dist, min_dist_idx] =  findSecondMin(Dist(i,:));
     if(Dist(i,min_dist_idx) < MAX_DISTANCE)
         if line(i).flag == 0
             line(i).kind = min_dist_idx;
@@ -42,11 +42,75 @@ for i = 1 : distNum
 end
 [cluster, ia , kind_idx] = unique(kind_array);
 
+kind_Nums = cell(1,length(cluster));
+for i = 1 : length(cluster)
+    clu = cluster(i);
+    Num = find(kind_array == clu);
+    kind_Nums{i} = Num';
+end
+
+load line.mat
+
+for i = 1:length(cluster)
+    plot_line = kind_Nums{i};
+    for j = 1 : size(plot_line)
+        m = plot_line(j);
+        [~,tmpLine] = size(line{m});
+%         x = line{m}(1,1:tmpLine);
+%         y = line{m}(2,1:tmpLine);
+%         z = line{m}(3,1:tmpLine);
+        x = line{m}(1,1);
+        y = line{m}(2,1);
+        z = line{m}(3,1);
+        if (i < 100)
+            plot3(x,y,z,'color',[i*0.01 0 0]);
+        elseif(100 <= i && i < 200)
+            plot3(x,y,z,'color',[(i-100)*0.01 (i-100)*0.01 0]);
+        elseif(200 <= i && i < 300)
+            plot3(x,y,z,'color',[(i-200)*0.01 (i-200)*0.01 (i-200)*0.01]);
+        elseif(300 <= i && i < 400)
+            plot3(x,y,z,'color',[(i-300)*0.01 0 (i-300)*0.01]);
+        elseif(400 <= i && i < 500)
+            plot3(x,y,z,'color',[(i-400)*0.01 0 (i-400)*0.01]);
+        elseif(500 <= i && i < 600)
+            plot3(x,y,z,'color',[0 0 (i-500)*0.01]);
+        elseif(600 <= i && i < 700)
+            plot3(x,y,z,'color',[0 (i-600)*0.01 0]);
+        elseif(700 <= i && i < 800)
+            plot3(x,y,z,'color',[0 (i-700)*0.01 0]);
+        elseif(800 <= i && i < 900)
+            plot3(x,y,z,'color',[(i-800)*0.01 (i-800)*0.01 (i-800)*0.01]);
+        elseif(900 <= i && i < 1000)
+            plot3(x,y,z,'color',[(i-900)*0.01 (i-900)*0.01 (i-900)*0.01]);
+        elseif(1000 <= i && i < 1100)
+            plot3(x,y,z,'color',[(i-1000)*0.01 (i-1000)*0.01 (i-1000)*0.01]);
+        elseif(1100 <= i && i < 1200)
+            plot3(x,y,z,'color',[(i-1100)*0.01 (i-1100)*0.01 (i-1100)*0.01]);
+        elseif(1200 <= i && i < 1300)
+            plot3(x,y,z,'color',[(i-1200)*0.01 (i-1200)*0.01 (i-1200)*0.01]);
+        elseif(1300 <= i && i < 1400)
+            plot3(x,y,z,'color',[(i-1300)*0.01 (i-1300)*0.01 (i-1300)*0.01]);
+        else
+            plot3(x,y,z,'color',[(i-1400)*0.01 (i-1400)*0.01 (i-1400)*0.01]);
+        end
+        hold on
+    end
+end
+
+
+
+%% 打印输出类别信息
+fprintf("总共有 %d 类:\n", length(cluster));
+for i = 1 : distNum
+    fprintf("第 %d 条曲线属于 第 %d 类\n",i, kind_idx(i));
+end
+
+toc
 
 
 
 
-function [secondMin, idx] =findSecondMin(distance,min_idx)
+function [secondMin, idx] =findSecondMin(distance)
     dist = distance;
     distNum = length(dist);
     for i = 1 : distNum-1
@@ -58,8 +122,6 @@ function [secondMin, idx] =findSecondMin(distance,min_idx)
             end
         end
     end
-    if (min_idx < distNum)
-        secondMin = dist(min_idx+1);
-    end
+        secondMin = dist(2);
     idx = find(distance == secondMin);
 end
